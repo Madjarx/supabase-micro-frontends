fb0cba7a1915c2cbfd8383a3bc2e031a349aff3c
Luka Radisavljevic <88193583+Madjarx@users.noreply.github.com>
2023-01-21 00:36:54 +0100

    Monorepo architecture

--- packages/remote/vite.config.js
+++ packages/remote/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'remote',
+      filename: 'remoteEntry.js',
+      // Modules to expose
+      exposes: {
+        './Button' : './src/components/Button.jsx'
+      },
+      shared: ['react']
+    })
+  ],
+  preview: {
+    host: 'localhost',
+    port: 5001,
+    // We're dealing with a lot of ports, have this option to true
+    // and make your life easier
+    strictPort: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
--- packages/shell/vite.config.js
+++ packages/shell/vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'shell',
+      remotes: {
+        // Make sure that the port specificed here is matching the preview port in the
+        // Remote source that you intend to import (so not a dev server port!!!)
+        shared: 'http://localhost:5001/assets/remoteEntry.js',
+      },
+      shared: ['react']
+    })
+  ],
+  preview: {
+    host: 'localhost',
+    port: 5000,
+    strictPort: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
