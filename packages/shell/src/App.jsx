cf1767fad033edf222073ead69224f9830603cc2
Luka Radisavljevic <88193583+Madjarx@users.noreply.github.com>
2023-01-21 00:49:35 +0100

    supabase session sharing init; nodemon rebuild script

--- packages/shell/src/App.jsx
+++ packages/shell/src/App.jsx
@@ -3,14 +3,25 @@ import './App.css'
 //This is the mife import
 import Button from 'shared/Button'
 
+import { supabase } from './supabase'
+
+
 function App() {
 
+  const handleLogin = async () => {
+    let { data, error } = await supabase.auth.signInWithPassword({
+      email: 'lukaradisavljevic30@gmail.com',
+      password: 'luka2002'
+    })
+  }
 
   return (
     <div className="App">
       <div>This is the Shell app</div>
       <hr />
       <div>This is the Remote import</div>
+      <hr />
+      <button onClick={handleLogin}>Click me to login</button>
       <hr />
       <Button></Button>
     </div>
---
+++ scripts/rebuild-remote.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# I want my nodemon script to look for changes in all the remotes (or only one remote app)
+# and instatly rebuild it and preview it, so that my shell app doesnt fail all the time
+
+# this is a dev script lol
